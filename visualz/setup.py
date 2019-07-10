f47af03cc9a3a0a67b3671c02f19332cddb0efff
Rising Odegua <williamsrising592@gmail.com>
2019-07-10 20:15:51 +0100

    Add twine files and .gitignore

--- visualz/setup.py
+++ visualz/setup.py
@@ -6,6 +6,34 @@ setup(name='visualz',
       url='https://github.com/risenW/visualz',
       author='Rising Odegua',
       author_email='risingodegua@gmail.com',
+      download_url='https://github.com/risenW/visualz/archive/v0.2-alpha.tar.gz', 
       license='MIT',
       packages=['visualz'],
       zip_safe=False)
+
+
+#       setup(
+#     name="gitcen",
+#     version='0.1.0',
+#     description="A project to find git information.",
+#     long_description="A project to find git information about authors' commits,most active days and time.",
+#     author="Anwesha Das.",
+#     author_email="anwesha@das.community",
+#     url="https://github.com/anweshadas/gitcen",
+#     license="GPLv3+",
+#     py_modules=['gitcen'],
+#     install_requires=[
+#         'Click',
+#         'pygit2==0.24'
+#     ],
+#     entry_points='''
+#         [console_scripts]
+#         gitcen=gitcen:main
+#     ''',
+#     classifiers=(
+#         'Development Status :: 3 - Alpha',
+#         'Intended Audience :: Developers',
+#         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+#         'Programming Language :: Python :: 3.6'
+#     )
+# )
