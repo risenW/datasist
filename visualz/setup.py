99b86f026fc42419d7ce268b8602ca10124f4786
Rising Odegua <williamsrising592@gmail.com>
2019-07-10 19:27:59 +0100

    Add basic setup.py information

---
+++ visualz/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='visualz',
+      version='0.1',
+      description='A data visualization library for scientist',
+      url='https://github.com/risenW/visualz',
+      author='Rising Odegua',
+      author_email='risingodegua@gmail.com',
+      license='MIT',
+      packages=['visualz'],
+      zip_safe=False)
---
+++ visualz/visualz/__init__.py
@@ -0,0 +1,2 @@
+def print_test():
+    return "Hello world"
