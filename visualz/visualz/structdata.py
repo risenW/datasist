9efdb7d945b5edfc99efde267a8b5855849cebf3
Rising Odegua <williamsrising592@gmail.com>
2019-07-11 21:11:47 +0100

    Add violin_num_2_cat function and updated the docs

--- visualz/visualz/structdata.py
+++ visualz/visualz/structdata.py
@@ -70,6 +70,8 @@ def box_num_2_cat(data=None, num_feats=None, target=None, fig_size=(5,5), large_
     large_data: boolean
             If True, then sns boxenplot is used instead of normal boxplot. Boxenplot is 
             better for large dataset
+    save_fig: boolean
+            If True, saves the current plot to the current working directory
     '''
 
     if target is None:
@@ -110,3 +112,50 @@ def box_num_2_cat(data=None, num_feats=None, target=None, fig_size=(5,5), large_
             if save_fig:
                 plt.savefig('fig_{}_vs_{}'.format(feat,target))
             plt.show()
+
+
+
+
+def violin_num_2_cat(data=None, num_feats=None, target=None, fig_size=(5,5), save_fig=False):
+    '''
+    Makes a violin plot of all numerical features against a specified categorical target column.
+ 
+    Parameters
+    ------------
+
+    data : Pandas dataframe.
+    num_feats: Scalar, array, or list. 
+               The numerical features in the dataset, if not provided, 
+               we try to infer the numerical columns from the dataframe.
+    target: array, pandas series, list.
+            A categorical target column. Maximun number of categories is 7 and minimum is 1
+    fig_size: tuple
+              The size of the figure object
+    save_fig: boolean
+            If True, saves the current plot to the current working directory
+    '''
+
+    if target is None:
+        raise ValueError('Target value cannot be None')
+
+    if len(data[target].unique()) > 7:
+        raise AttributeError("Target categories must be less than seven")
+
+
+    if num_feats is None:
+        #TODO: Get numerical features from data
+        pass
+
+    for feat in num_feats:
+        fig = plt.figure(figsize=fig_size)
+        ax = fig.gca()
+
+        sns.set_style("whitegrid")
+        sns.violin(target, feat, data=data, ax=ax)
+        plt.xlabel(feat) # Set text for the x axis
+        plt.ylabel(target)# Set text for y axis
+        plt.title('Violin plot of {} against {}'.format(feat, target))
+        if save_fig:
+            #TODO Add function to save to a specified directory
+            plt.savefig('fig_{}_vs_{}'.format(feat,target))
+        plt.show()
