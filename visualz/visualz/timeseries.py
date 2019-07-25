19120df2d3383aa1992cdd006675d96ce9d86508
Rising Odegua <williamsrising592@gmail.com>
2019-07-25 18:12:07 +0100

    Add num_to_time function that plots numerical features to time index

--- visualz/visualz/timeseries.py
+++ visualz/visualz/timeseries.py
@@ -50,3 +50,48 @@ def describe_date(data, date_feat):
 
     df = get_date_info(data, date_feat)
     print(df.describe())
+
+
+def num_to_time(data=None, num_features=None,time_col=None, subplots=True, marker='.', figsize=(15,10), y_label='Daily Totals',save_fig=False, alpha=0.5, linestyle='None'):
+    '''
+    Plots all numeric features against the time. Interpreted as a time series plot
+
+    Parameters:
+    #TODO UPDATE DOC
+    
+    '''
+
+    if data is None:
+        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
+
+    if num_features is None:
+        num_features = datastats.get_num_feats(data)
+
+    if time_col is None:
+        raise ValueError("time_col: Expecting a Datetime Series, got 'None'")
+
+    #Make time_col the index
+    data[time_col] = pd.to_datetime(data[time_col])
+    #Set as time_col as DataFrame index
+    data = data.set_index(time_col)
+    
+    if subplots:
+        axes = data[num_features].plot(marker=marker,subplots=True, figsize=figsize, alpha=0.5, linestyle=linestyle) 
+        for feature, ax in zip(num_features, axes):
+            ax.set_ylabel(y_label)
+            ax.set_title("Timeseries Plot of '{}'".format(time_col))
+            if save_fig:
+                plt.savefig('fig_timeseries_plot_against_{}'.format(feature))
+            plt.show()
+    else:
+        for feature in num_features:
+            fig = plt.figure()
+            ax = fig.gca()
+            axes = data[feature].plot(marker=marker,subplots=False, figsize=figsize, alpha=0.5, linestyle=linestyle, ax=ax) 
+            plt.ylabel(feature)
+            ax.set_title("Timeseries Plot of '{}' vs. '{}' ".format(time_col, feature))
+            if save_fig:
+                plt.savefig('fig_timeseries_plot_against_{}'.format(feature))
+            plt.show()           
+
+
