0d075702556f665a6b1d719085b208edc4e9ab0d
risingodegua <williamsrising592@gmail.com>
2019-08-14 15:54:45 +0100

    Removed OOP implementation and rename some modules

--- DataSist/datasist/structdata.py
+++ DataSist/datasist/structdata.py
@@ -1,380 +1,139 @@
+import numpy as numpy
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from . import datastats
+from .visualizations import class_in_cat_feature, plot_missing
 
 
-class Structdata:
+def describe(data=None, name='', date_cols=None, show_categories=False, plot_missing=False):
+    '''
+    Calculates statistics and information about a data set. Information like
+    shapes, size, number of categorical/numeric or date features, number of missing values
+    data types of objects e.t.c
 
-    def __init__(self, data, fig_size=(5,5), save_fig=False):
-        self.data = data
-        self.fig_size = fig_size
-        self.save_fig = save_fig
-
-
-
-    def bar_cat_features(self, data=None, cat_features=None, separate_by=None, fig_size=(5,5), save_fig=False):
-        '''
-        Makes a bar plot of all categorical features to show their counts.
-        
-        Parameters
-        ------------
-
-        data : Pandas dataframe.
-        cat_features: Scalar, array, or list. 
-                The categorical featureures in the dataset, if not provided, 
-                we try to infer the categorical columns from the dataframe.
-        fig_size: tuple
-                The size of the figure object
-        '''
-
-        data = self.data if data is None else data
-
-        # if data is None:
-        #     raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
-
-        if cat_features is None:
-            cat_features = datastats.get_cat_feats(data)
-            
-        for feature in cat_features:
-            #Check the size of categories in the feature: Anything greater than 20 is not plotted
-            if len(data[feature].unique()) > 30:
-                print("Unique Values in {} is too large to plot".format(feature))
-                print('\n')
-            else:
-                fig = plt.figure(figsize=fig_size)
-                ax = fig.gca()
-
-                sns.countplot(x=feature, hue=separate_by, data=data)
-                plt.xticks(rotation=90)
-                ax.set_title("Bar plot for " + feature)
-
-                if save_fig:
-                    plt.savefig('Barplot_{}'.format(feature))
-
-
-
-
-
-    def box_num_2_cat_target(self, data=None, num_features=None, target=None, fig_size=(5,5), large_data=False, save_fig=False):
-        '''
-        Makes a box plot of all numerical featureures against a specified categorical target column.
+    Parameters:
+    data: Pandas DataFrame
+        The data to describe
+    '''
     
-        Parameters
-        ------------
+    if data is None:
+        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
 
-        data : Pandas dataframe.
-        num_features: Scalar, array, or list. 
-                The numerical featureures in the dataset, if not provided, 
-                we try to infer the numerical columns from the dataframe.
-        target: array, pandas series, list.
-                A categorical target column. Maximun number of categories is 7 and minimum is 1
-        fig_size: tuple
-                The size of the figure object
-        large_data: boolean
-                If True, then sns boxenplot is used instead of normal boxplot. Boxenplot is 
-                better for large dataset
-        save_fig: boolean
-                If True, saves the current plot to the current working directory
-        '''
-
-        data = self.data if data is None else data
-
-        if target is None:
-            raise ValueError('Target value cannot be None')
-
-        if len(data[target].unique()) > 7:
-            raise AttributeError("Target categories must be less than seven")
-
-        # if data is None:
-        #     raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
-
-        if num_features is None:
-            num_features = datastats.get_num_feats(data)
-        
-        if large_data:
-            #use advanced sns boxenplot
-            for feature in num_features:
-                fig = plt.figure(figsize=fig_size)
-                ax = fig.gca()
-
-                sns.set_style("whitegrid")
-                sns.boxenplot(target, feature, data=data, ax=ax)
-                plt.ylabel(feature) # Set text for the x axis
-                plt.xlabel(target)# Set text for y axis
-                plt.xticks(rotation=90)
-                plt.title('Box plot of {} against {}'.format(feature, target))
-                if save_fig:
-                    plt.savefig('fig_{}_vs_{}'.format(feature,target))
-                plt.show()
-        else:
-            for feature in num_features:
-                fig = plt.figure(figsize=fig_size)
-                ax = fig.gca()
-
-                sns.set_style("whitegrid")
-                sns.boxplot(target, feature, data=data, ax=ax)
-                plt.ylabel(feature) # Set text for the x axis
-                plt.xlabel(target)# Set text for y axis
-                plt.xticks(rotation=90)
-                plt.title("Box plot of '{}' vs. '{}'".format(feature, target))
-                if save_fig:
-                    plt.savefig('fig_{}_vs_{}'.format(feature,target))
-                plt.show()
-
-
-
-
-    def violin_num_2_cat_target(self, data=None, num_features=None, target=None, fig_size=(5,5), save_fig=False):
-        '''
-        Makes a violin plot of all numerical featureures against a specified categorical target column.
+    ## Get categorical features
+    cat_features = get_cat_feats(data)
     
-        Parameters
-        ------------
-
-        data : Pandas dataframe.
-        num_features: Scalar, array, or list. 
-                The numerical featureures in the dataset, if not provided, 
-                we try to infer the numerical columns from the dataframe.
-        target: array, pandas series, list.
-                A categorical target column. Maximun number of categories is 7 and minimum is 1
-        fig_size: tuple
-                The size of the figure object
-        save_fig: boolean
-                If True, saves the current plot to the current working directory
-        '''
-
-        data = self.data if data is None else data
-
-        if target is None:
-            raise ValueError('Target value cannot be None')
-
-        if len(data[target].unique()) > 7:
-            raise AttributeError("Target categories must be less than seven")
+    #Get numerical features
+    num_features = get_num_feats(data)
 
 
-        # if data is None:
-        #     raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
+    print('Shape of {} data set: {}'.format(name, data.shape))
+    _space()
+    print('Size of {} data set: {}'.format(name, data.size))
+    _space()
+    print('Data Types')
+    print("Note: All Non-numerical features are identified as objects")
+    print(data.dtypes)
+    _space()
+    print('Numerical Features in Data set')
+    print(num_features)
+    _space()
+    print('Statistical Description of Numerical Columns')
+    print(data.describe())
+    _space()
+    print('Categorical Features in Data set')
+    print(cat_features)
+    _space()
+    print('Unique class Count of Categorical features')
+    print(get_unique_counts(data))
+    _space()
 
-        if num_features is None:
-            num_features = datastats.get_num_feats(data)
+    if show_categories:     
+        print('Classes in Categorical Columns')
+        print(class_in_cat_feature(data, cat_features))
+        _space()
 
-        for feature in num_features:
-            fig = plt.figure(figsize=fig_size)
-            ax = fig.gca()
+    print('Missing Values in Data')
+    print(display_missing(data))
 
-            sns.set_style("whitegrid")
-            sns.violinplot(target, feature, data=data, ax=ax)
-            plt.xticks(rotation=90)
-            plt.ylabel(feature) # Set text for the x axis
-            plt.xlabel(target)# Set text for y axis
-            plt.title("Violin plot of '{}' vs. '{}'".format(feature, target))
-            if save_fig:
-                #TODO Add function to save to a specified directory
-                plt.savefig('fig_{}_vs_{}'.format(feature,target))
-            plt.show()
+    #Plots the missing values
+    if plot_missing:
+        plot_missing(data)
 
 
 
+def get_cat_feats(data=None):
+    '''
+    Returns the categorical features in a data set
+    '''
+    if data is None:
+        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
 
-    def hist_num_features(self, data=None, num_features=None, bins=None, show_dist_type=False, fig_size=(5,5), save_fig=False):
-        '''
-        Makes an histogram plot of all numerical featureures. Helps to show the distribution of the featureures.
-        
+    cat_features = []
+    for col in data.columns:
+        if data[col].dtypes == 'object':
+            cat_features.append(col)
+
+    return cat_features
+
+
+def get_num_feats(data=None):
+    '''
+    Returns the numerical features in a data set
+    '''
+    if data is None:
+        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
     
-        Parameters
-        ------------
-
-        data : Pandas dataframe.
-        num_features: Scalar, array, or list. 
-                The numerical featureures in the dataset, if not provided, 
-                we try to infer the numerical columns from the dataframe.
-        bins: int
-                The number of bins to use.
-        show_dist_type: boolean
-                If True, Calculates the skewness of the data and display one of (Left skewed, right skewed or normal) 
-        fig_size: tuple
-                The size of the figure object
-        save_fig: boolean
-                If True, saves the current plot to the current working directory
-        '''
-
-        data = self.data if data is None else data
-
-        # if data is None:
-        #     raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
-
-        if num_features is None:
-            num_features = datastats.get_num_feats(data)
-
-        for feature in num_features:
-            fig = plt.figure(figsize=fig_size)
-            ax = fig.gca()
-
-            sns.distplot(data[feature].values, ax=ax, bins=bins)
-            ax.set_xlabel(feature) # Set text for the x axis
-            ax.set_ylabel('Count')# Set text for y axis
-
-            if show_dist_type:
-                ##TODO Add Code to calculate skewness
-                pass
-            else:
-                ax.set_title('Histogram of ' + feature)
-
-            if save_fig:
-                #TODO Add function to save to a specified directory
-                plt.savefig('fig_hist_{}'.format(feature))
-
-            plt.show()
-
-
-
-    def bar_cat_2_cat_target(self, data=None, cat_features=None, target=None, fig_size=(12,6), save_fig=False):
-        '''
-        Makes a side by side bar plot of all categorical features against the target classes.
-        
+    num_features = []
+    for col in data.columns:
+        if data[col].dtypes != 'object':
+            num_features.append(col)
     
-        Parameters
-        ------------
+    return num_features
 
-        data : Pandas dataframe.
-                The data we are working with.
-        cat_features: Scalar, array, or list. 
-                The categorical features in the dataset, if not provided, 
-                we try to infer the categorical columns from the dataframe.
-        fig_size: tuple
-                The size of the figure object
-        save_fig: boolean
-                If True, saves the current plot to the current working directory
-        '''
-        data = self.data if data is None else data
 
-        # if data is None:
-        #     raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
 
-        if cat_features is None:
-            cat_features = datastats.get_cat_feats(data)
 
-        #remove target from cat_features
-        try:
-            cat_features.remove(target)
-        except:
-            pass
+def get_unique_counts(data=None):
+    '''Gets the unique count of elements in a data set'''
+
+    if data is None:
+        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
+
+    features = get_cat_feats(data)
+    temp_len = []
+
+    for feature in features:
+        temp_len.append(len(data[feature].unique()))
         
-        if len(data[target].unique()) > 7:
-            raise AttributeError("Target categories must be less than seven")
-
-        #Create a dummy column to hold count of values
-        data['dummy_count'] = np.ones(shape = data.shape[0])
-        #Loop over each categorical featureure and plot the acceptance rate for each category.
-        for feature in cat_features:
-            #Plots are made for only categories with less than 10 unique values because of speed
-            if len(data[feature].unique()) > 10 :
-                print("{} feature has too many categories and will not be ploted".format(feature))
-                
-            else:     
-                counts = data[['dummy_count', target, feature]].groupby([target, feature], as_index = False).count()
-                #get the categories
-                cats = list(data[target].unique())
-
-                if len(cats) > 6:
-                    raise ValueError("Target column: '{}' must contain less than six unique classes".format(target))
-
-                #create new figure
-                _ = plt.figure(figsize = fig_size)
-
-                for i, cat in enumerate(cats): 
-                    plt.subplot(1, len(cats), i+1)
-                    #Get the counts each category in target     
-                    temp = counts[counts[target] == cat][[feature, 'dummy_count']] 
-                    sns.barplot(x=feature, y='dummy_count', data=temp)
-                    plt.xticks(rotation=90)
-                    plt.title('Counts for {} \n class {}'.format(feature, cat))
-                    plt.ylabel('count')
-                    plt.tight_layout(2)
-
-                    if save_fig:
-                        plt.savefig('fig_cat_2_cat_target_{}'.format(feature))
+    dic = list(zip(features, temp_len))
+    dic = pd.DataFrame(dic, columns=['Feature', 'Unique Count'])
+    return dic
 
 
-        #Drop the dummy_count column from data
-        data.drop(['dummy_count'], axis=1, inplace = True)
+def display_missing(data=None, plot=True):
+    '''
+    Display missing values as a pandas dataframe.
+    '''
+
+    if data is None:
+        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
+
+    df = data.isna().sum()
+    df = df.reset_index()
+    df.columns = ['features', 'missing_counts']
+
+    missing_percent = round((df['missing_counts'] / data.shape[0]) * 100, 2)
+    df['missing_percent'] = missing_percent
+
+    if plot:
+        plot_missing()
+        return df
+    else:
+        return df
 
 
-    def class_in_cat_feature(self, data=None, cat_features=None, plot=False, save_fig=False):
-        '''
-        Prints the categories and counts of a categorical feature
-
-        Parameters:
-        
-        data: Pandas DataFrame or Series
-        cat_features: Scalar, array, or list. 
-                The categorical features in the dataset, if not provided, 
-                we try to infer the categorical columns from the dataframe.
-        '''
-        data = self.data if data is None else data
-
-        # if data is None:
-        #     raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
-
-        if cat_features is None:
-            cat_features = datastats.get_cat_feats(data)
-
-                            
-
-        for feature in cat_features:
-            print('Class Count for', feature)
-            print(data[feature].value_counts())
-            print("-----------------------------")
-
-        if plot:
-            bar_cat_features(data, cat_features, save_fig=save_fig)
-
-
-
-    def scatter_num_2_num_feat(self, data=None, num_features=None, target=None, separate_by=None, fig_size=(10,10), save_fig=False):
-        '''
-        Makes an histogram plot of all numerical featureures. Helps to show the distribution of the featureures.
-        
-    
-        Parameters
-        ------------
-
-        data : Pandas dataframe.
-        num_features: Scalar, array, or list. 
-                The numerical featureures in the dataset, if not provided, 
-                we try to infer the numerical columns from the dataframe.
-        separate_by: string
-                The cateogical feature to use in class separation. Also called 'hue' in seaborn
-        fig_size: tuple
-                The size of the figure object
-        save_fig: boolean
-                If True, saves the current plot to the current working directory
-        '''
-
-        data = self.data if data is None else data
-
-        # if data is None:
-        #     raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
-
-        if separate_by == '':
-            pass
-        else:
-            if separate_by not in data.columns:
-                raise ValueError("{} not found in data columns".format(separate_by))
+def _space():
+    print('-' * 100)
+    print('\n')
 
         
-        if target is None:
-            raise ValueError('Target value cannot be None')
-
-        if num_features is None:
-            num_features = datastats.get_num_feats(data)
-
-        for feature in num_features:
-            fig = plt.figure(figsize=fig_size) # define plot area
-            ax = fig.gca() # define axis  
-            sns.scatterplot(x=feature, y=target, data=data, hue=separate_by)
-            ax.set_title("Scatter Plot of '{}' vs. '{}' \n Separated by: '{}'".format(feature, target, separate_by))
-            if save_fig:
-                plt.savefig('fig_num_2_num_{}'.format(feature))
--- DataSist/setup.py
+++ DataSist/setup.py
@@ -1,39 +1,20 @@
 from setuptools import setup
 
-setup(name='visualz',
+setup(name='Datasist',
       version='0.1',
-      description='A data visualization library for scientist',
+      description='A Machine learning library that abstracts repetitve functions used by data scientist and machine learning engineers',
       url='https://github.com/risenW/visualz',
       author='Rising Odegua',
       author_email='risingodegua@gmail.com',
       download_url='https://github.com/risenW/visualz/archive/v0.2-alpha.tar.gz', 
       license='MIT',
-      packages=['visualz'],
-      zip_safe=False)
-
-
-#   setup(
-#     name="visualz",
-#     version='0.1',
-#     description="'A data visualization library for quick and easy data exploration",
-#     long_description="A visualizaion library that offers great exploration for shorter lines of codes.",
-#     author="Anwesha Das.",
-#     author_email="anwesha@das.community",
-#     url="https://github.com/anweshadas/gitcen",
-#     license="GPLv3+",
-#     py_modules=['gitcen'],
-#     install_requires=[
-#         'Click',
-#         'pygit2==0.24'
-#     ],
-#     entry_points='''
-#         [console_scripts]
-#         gitcen=gitcen:main
-#     ''',
-#     classifiers=(
-#         'Development Status :: 3 - Alpha',
-#         'Intended Audience :: Developers',
-#         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-#         'Programming Language :: Python :: 3.6'
-#     )
-# )
+      install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'numpy',
+        'warnings'
+    ],
+      zip_safe=False
+      
+      )
