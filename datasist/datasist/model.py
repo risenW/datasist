d16afc115a9860073fb1cabad8d01b2b63987633
risingodegua <williamsrising592@gmail.com>
2019-08-14 16:38:14 +0100

    Fixed error check in num_2_num plot

---
+++ datasist/datasist/model.py
@@ -0,0 +1,105 @@
+'''
+This module contains all functions relating to modeling in using sklearn library.
+
+'''
+
+from sklearn.metrics import roc_curve,confusion_matrix, precision_score,accuracy_score, recall_score,f1_score, make_scorer
+from sklearn.model_selection import KFold, cross_val_score
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def train_classifier(train_data = None, target=None, val_data=None, val_data_target=None, model=None, cross_validate=False, cv=5, show_roc_plot=True, save_plot=False):
+    '''
+    train a classification model and returns all the popular performance
+    metric
+    Parameters:
+    #TODO update Doc
+    
+'''
+    if train_data is None:
+        raise ValueError("train_data: Expecting a DataFrame/ numpy2d array, got 'None'")
+    
+    if target is None:
+        raise ValueError("target: Expecting a Series/ numpy1D array, got 'None'")
+
+    #initialize variables to hold calculations
+    pred, acc, f1, precision, recall, confusion_mat = 0, 0, 0, 0, 0, None
+
+    if cross_validate:
+        dict_scorers  = {'acc' : accuracy_score,
+                    'f1' : f1_score,
+                    'precision': precision_score, 
+                    'recall' : recall_score}
+
+        metric_names = ['Accuracy', 'F1_score', 'Precision', 'Recall']
+
+        for metric_name, scorer in zip(metric_names, dict_scorers):
+            cv_score = np.mean(cross_val_score(model, train_data, target, scoring=make_scorer(dict_scorers[scorer]),cv=cv))
+            print("{} is {}".format(metric_name,  round(cv_score * 100, 4)))
+        #TODO Add cross validation function for confusion matrix
+  
+    else:
+        if val_data is None:
+            raise ValueError("val_data: Expecting a DataFrame/ numpy2d array, got 'None'")
+        
+        if val_data_target is None:
+            raise ValueError("val_data_target: Expecting a Series/ numpy1D array, got 'None'")
+
+        model.fit(train_data, target)
+        pred = model.predict(val_data)
+        get_classification_report(val_data_target, pred, show_roc_plot, save_plot)
+
+
+def train_predict(model=None, train_data=None, target=None, test_data=None, make_submission_file=False,
+                  sample_submision_file=None, submission_col_name=None, 
+                  submision_file_name=None):
+    '''
+    Train and model and makes prediction with it on the final test set. Also
+    returns a sample submission file for data science competitions
+    
+    Parameters:
+
+    '''
+    model.fit(train_data, target)
+    pred = model.predict(test_data)
+
+    if make_submission_file:
+        sub = sample_submision_file
+        sub[submission_col_name] = pred
+        sub.to_csv(submision_file_name + '.csv', index=False)
+        print("File has been saved to current working directory")
+    else:
+        return pred
+
+
+def get_classification_report(target=None, pred=None, show_roc_plot=True, save_plot=False):
+    acc = accuracy_score(target, pred)
+    f1 = f1_score(target, pred)
+    precision = precision_score(target, pred)
+    recall = recall_score(target, pred)
+    confusion_mat = confusion_matrix(target, pred)
+
+    print("Accuracy is ", round(acc * 100))
+    print("F1 score is ", round(f1 * 100))
+    print("Precision is ", round(precision * 100))
+    print("Recall is ", round(recall * 100))
+    print("*" * 100)
+    print("confusion Matrix")
+    print('                 Score positive    Score negative')
+    print('Actual positive    %6d' % confusion_mat[0,0] + '             %5d' % confusion_mat[0,1])
+    print('Actual negative    %6d' % confusion_mat[1,0] + '             %5d' % confusion_mat[1,1])
+    print('')
+
+    if show_roc_plot:        
+        fpr, tpr, thresholds = roc_curve(target, pred)
+        plt.plot([0, 1], [0, 1], linestyle='--')
+        # plot the roc curve for the model
+        plt.plot(fpr, tpr, marker='.')
+        # show the plot
+        plt.title("roc curve")
+        plt.show()
+
+        if save_plot:
+            plt.savefig("roc_plot.png")
---
+++ datasist/datasist/timeseries.py
@@ -0,0 +1,144 @@
+'''
+This module contains all functions relating to timeseries data
+
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def get_date_info(data=None, date_features=None, date_cols_to_return=None, drop_date_feature=True):
+    '''
+    TODO UPdate Doc
+    Returns the date information from a given date column
+    
+    '''
+    df = data.copy()
+
+    for date_feature in date_features:
+        #Convert date feature to Pandas DateTime
+        df[date_feature]=pd.to_datetime(df[date_feature])
+
+        #specify columns to return
+        dict_dates = {  "dow":  df[date_feature].dt.weekday_name,
+                        "doy":   df[date_feature].dt.dayofyear,
+                        "dom": df[date_feature].dt.day,
+                        "hr": df[date_feature].dt.hour,
+                        "minute":   df[date_feature].dt.minute,
+                        "is_wkd":  df[date_feature].apply( lambda x : 1 if x  in [5,6] else 0 ),
+                        "yr": df[date_feature].dt.year,
+                        "qtr":  df[date_feature].dt.quarter,
+                        "mth": df[date_feature].dt.month
+                    } 
+        date_fts = ['dow', 'doy', 'dom', 'hr', 'minute', 'is_wkd', 'yr', 'qtr', 'mth']
+
+        if date_cols_to_return is None:
+            #return all features
+            for dt_ft in date_fts:
+                df[date_feature + '_' + dt_ft] = dict_dates[dt_ft]
+        else:
+            #Return only sepcified date features
+            for dt_ft in date_cols_to_return:
+                df[date_feature + '_' + dt_ft] = dict_dates[dt_ft]
+    
+    if drop_date_feature:
+        df.drop(date_features, axis=1, inplace=True)
+
+    return df
+
+
+
+def describe_date(data, date_feature):
+    '''
+    Calculate statistics of the date feature
+    '''
+
+    df = get_date_info(data, date_feature)
+    print(df.describe())
+
+
+
+
+def num_to_time(data=None, num_features=None,time_col=None, subplots=True, marker='.', figsize=(15,10), y_label='Daily Totals',save_fig=False, alpha=0.5, linestyle='None'):
+    '''
+    Plots all numeric features against the time. Interpreted as a time series plot
+
+    Parameters:
+    #TODO UPDATE DOC
+    
+    '''
+
+    if data is None:
+        raise ValueError("data: Expecting a DataFrame or Series, got 'None'")
+
+    if num_features is None:
+        num_features = datastats.get_num_feats(data)
+        #remove the time_Col from num_features
+        num_features.remove(time_col)
+
+    if time_col is None:
+        raise ValueError("time_col: Expecting a Datetime Series, got 'None'")
+
+    #Make time_col the index
+    data[time_col] = pd.to_datetime(data[time_col])
+    #Set as time_col as DataFrame index
+    data = data.set_index(time_col)
+    
+    if subplots:
+        axes = data[num_features].plot(marker=marker,subplots=True, figsize=figsize, alpha=0.5, linestyle=linestyle) 
+        for feature, ax in zip(num_features, axes):
+            ax.set_ylabel(y_label)
+            ax.set_title("Timeseries Plot of '{}'".format(time_col))
+            if save_fig:
+                plt.savefig('fig_timeseries_plot_against_{}'.format(feature))
+            plt.show()
+    else:
+        for feature in num_features:
+            fig = plt.figure()
+            ax = fig.gca()
+            axes = data[feature].plot(marker=marker,subplots=False, figsize=figsize, alpha=0.5, linestyle=linestyle, ax=ax) 
+            plt.ylabel(feature)
+            ax.set_title("Timeseries Plot of '{}' vs. '{}' ".format(time_col, feature))
+            if save_fig:
+                plt.savefig('fig_timeseries_plot_against_{}'.format(feature))
+            plt.show()           
+
+
+
+def box_time_plot(data=None, features=None, x=None, subplots=True, figsize=(12,10)):
+    '''
+    Makes a box plot of features against a specified column
+    
+    '''
+
+    if subplots:
+        fig, axes = plt.subplots(len(features), 1, figsize=figsize, sharex=True)
+        for feature, ax in zip(features, axes):
+            sns.boxplot(data=data, x=x, y=feature, ax=ax)
+            ax.set_ylabel('Count')
+            ax.set_title("Boxplot of '{}' vs. {} ".format(feature, x))
+            plt.tight_layout()
+            # Remove the automatic x-axis label from all but the bottom subplot
+            if ax != axes[-1]:
+                ax.set_xlabel('')
+    else:
+        for feature in features:
+            fig = plt.figure(figsize=figsize) # define plot area
+            ax = fig.gca() # define axis 
+            sns.boxplot(data=data, x=x, y=feature, ax=ax)
+            ax.set_ylabel('Count')
+            ax.set_title("Boxplot of '{}' vs. {} ".format(feature, x))
+            plt.tight_layout()
+            ax.set_xlabel('')
+            plt.show()
+
+
+
+def set_date_index(data, date_feature):
+    #Make time_col the index
+    data[date_feature] = pd.to_datetime(data[date_feature])
+    #Set as time_col as DataFrame index
+    return data.set_index(date_feature)
+
