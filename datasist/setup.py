4758fc5f2cbace5b9a699c011e74e40d1d3a54e0
risenW <risingodegua@gmail.com>
2019-10-30 09:13:13 +0100

    Update Setup files

--- datasist/setup.py
+++ datasist/setup.py
@@ -1,20 +1,29 @@
 from setuptools import setup
 
-setup(name='datasist',
-      version='0.2',
+setup(
+      name='datasist',
+      packages=['datasist'],
+      version='0.1',
+      license='MIT',
       description='A Machine learning library that abstracts repetitve functions used by data scientist and machine learning engineers',
-      url='https://github.com/risenW/visualz',
       author='Rising Odegua',
       author_email='risingodegua@gmail.com',
-      download_url='https://github.com/risenW/visualz/archive/v0.2-alpha.tar.gz', 
-    #   license='MIT',
-    #   install_requires=[
-    #     'pandas',
-    #     'matplotlib',
-    #     'seaborn',
-    #     'numpy',
-    #     'warnings'
-    # ],
-      zip_safe=False
+      url='https://github.com/risenW/datasist/tree/master/datasist/datasist',
+      keywords=['Data Analysis', 'Feature Engineering', 'Visualization'],
+      download_url='https://github.com/risenW/datasist/archive/v_01.tar.gz', 
+      license='MIT',
+      install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'numpy'
+         ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',   
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3.7',
+      ],
       
       )
