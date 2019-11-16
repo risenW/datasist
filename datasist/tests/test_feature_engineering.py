e3af5e9f20a0df723b614359f35469f6a24b6321
risenW <risingodegua@gmail.com>
2019-11-16 14:06:42 +0100

    Update folder for tests

--- datasist/tests/test_feature_engineering.py
+++ datasist/tests/test_feature_engineering.py
@@ -1,7 +1,7 @@
 import pytest
-import datasist.feature_engineering as feature_engineering
 import pandas as pd
 import numpy as np
+import datasist as ds
 
 df = pd.DataFrame({'country': ['Nigeria', 'Ghana', 'USA', 'Germany'],
                     'size': [280, 20, 60, np.NaN],
@@ -18,46 +18,46 @@ df_dist = pd.DataFrame({'lat1': [10, 11, 12, 14],
 
 def test_drop_missing():
     expected = ['country']
-    output = list(feature_engineering.drop_missing(df2, 75).columns)
+    output = list(ds.feature_engineering.drop_missing(df2, 75).columns)
     assert expected == output
     
 def test_drop_missing_greater():
     expected = ['country', 'size']
-    output = list(feature_engineering.drop_missing(df2, 90).columns)
+    output = list(ds.feature_engineering.drop_missing(df2, 90).columns)
     assert expected == output
 
 def test_drop_redundant():
     expected = ['country', 'size']
-    output = list(feature_engineering.drop_redundant(df).columns)
+    output = list(ds.feature_engineering.drop_redundant(df).columns)
     assert expected == output
 
 def test_fill_missing_cats():
     expected = ['En']
-    output = feature_engineering.fill_missing_cats(df)['language']
+    output = ds.feature_engineering.fill_missing_cats(df)['language']
     assert expected == list(output.unique())
 
 def test_fill_missing_num_mean():
     expected = 120
-    output = feature_engineering.fill_missing_num(df)['size']
+    output = ds.feature_engineering.fill_missing_num(df)['size']
     assert expected == output[3]
 
 def test_harversine_distance():
     expected = [3253, 2029, 1093, 1722]
-    output = list(feature_engineering.haversine_distance(df_dist['lat1'], df_dist['lat2'], df_dist['long1'], df_dist['long2']).astype('int64'))
+    output = list(ds.feature_engineering.haversine_distance(df_dist['lat1'], df_dist['lat2'], df_dist['long1'], df_dist['long2']).astype('int64'))
     assert expected == output
     
 def test_manhattan_distance():
     expected = [37, 25, 13, 22]
-    output = list(feature_engineering.manhattan_distance(df_dist['lat1'], df_dist['lat2'], df_dist['long1'], df_dist['long2']).astype('int64'))
+    output = list(ds.feature_engineering.manhattan_distance(df_dist['lat1'], df_dist['lat2'], df_dist['long1'], df_dist['long2']).astype('int64'))
     assert expected == output
 
 def test_bearing():
     expected = [-106, -118, -155, -129]
-    output = list(feature_engineering.bearing(df_dist['lat1'], df_dist['lat2'], df_dist['long1'], df_dist['long2']).astype('int64'))
+    output = list(ds.feature_engineering.bearing(df_dist['lat1'], df_dist['lat2'], df_dist['long1'], df_dist['long2']).astype('int64'))
     assert expected == output
 
 def test_get_location_center():
     expected = [20, 15, 11, 17]
-    output = list(feature_engineering.get_location_center(df_dist['lat1'], df_dist['lat2']).astype('int64'))
+    output = list(ds.feature_engineering.get_location_center(df_dist['lat1'], df_dist['lat2']).astype('int64'))
     assert expected == output
 
