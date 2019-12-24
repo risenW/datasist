d39686290ea5f333d0af1cd060ca5c4fe651e45a
risenW <risingodegua@gmail.com>
2019-12-24 17:16:36 +0100

    Add test for get_period_of_day

--- datasist/tests/test_timeseries.py
+++ datasist/tests/test_timeseries.py
@@ -5,10 +5,18 @@ import numpy as np
 from datasist import timeseries
 
 
-def test_time_elapsed():
+def test_get_time_elapsed():
     date1 = ['2019-07-27 19:00', '2019-07-27 18:00', '2019-07-27 15:00']
     date2 = ['2019-07-27 20:00', '2019-07-27 20:00', '2019-07-27 18:00']
 
     expected = pd.DataFrame([3600.0, 7200.0, 10800.0]).values
-    output = timeseries.time_elapsed(date_cols=[date2, date1]).values
+    output = timeseries.get_time_elapsed(date_cols=[date2, date1]).values
     np.testing.assert_array_equal(expected,output)
+
+
+def test_get_period_of_day():
+    df = pd.Series([12,15,1,22,23,1])
+    expected = pd.Series(['morning', 'afternoon', 'morning', 'evening', 'evening', 'morning'])
+    output = timeseries.get_period_of_day(df)
+    pd.testing.assert_series_equal(expected, output)
+    
