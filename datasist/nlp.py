3faab2f5217f3f5044bd912bf444f123a195b919
rexsimiloluwah <adetoyosi2001@gmail.com>
2020-06-02 03:14:42 +0100

    Added some NLP functions

--- datasist/nlp.py
+++ datasist/nlp.py
@@ -46,3 +46,89 @@ def stemming(data):
 def flatten_list(list_of_list):
     flat_list = [item for sublist in list_of_list for item in sublist]
     return flat_list
+
+
+def sentence_similarity(sentence1, sentence2, stopwords = nltk.corpus.stopwords.words('english')):
+    
+    """ 
+    Finds the Lexical similarity between two sentences based on cosine similarity
+    
+    Parameters :-
+    ------------------------------------
+          sentence1 (str) : First sentence
+          sentence2 (str) : Second sentence that is compared to the first sentence
+          stopwords (list) : List of stopwords for filtering commonly used words
+          
+    Returns :-
+    -------------------------------------
+          similarity (float) : Similarity index between the two sentences represented in a probabilistic form (0.0 <= x <= 1.0)      
+    """
+    if stopwords == None:
+        stopwords = []
+        
+    assert len(sentence1) > 0 and len(sentence2) > 0, "Each sentence must contain at least one word !"
+    
+
+    sent1 = [word.lower() for word in sentence1.split(" ")]
+    sent2 = [word.lower() for word in sentence2.split(" ")]
+
+    all_words = list(set(sent1+sent2))
+    vect_array1 = np.zeros(len(all_words))
+    vect_array2 = np.zeros(len(all_words))
+
+    for word in sent1:
+        if word in stopwords:
+            continue
+        else:
+            vect_array1[all_words.index(word)] += 1
+
+    for word in sent2:
+        if word in stopwords:
+            continue
+        else:
+            vect_array2[all_words.index(word)] += 1
+    
+    similarity = 1 - cosine_distance(vect_array1, vect_array2)
+
+    return similarity
+
+
+def remove_newline(text):
+    """
+        Remove newline character and clean text particularly extracted from documents
+
+        Parameters :
+        --------------------------------------------------
+        text (str) : Text to be cleaned
+
+        Returns :
+        --------------------------------------------------
+        sentence
+    """
+    cleaned_text = []
+    for i in text.split():
+        if i == "\n":
+            continue
+        else: 
+            cleaned_text.append(i.replace("[^a-zA-Z]", ""))
+    cleaned_text = " ".join(cleaned_text)
+    cleaned_text = re.sub(r'\[[0-9]*\]', ' ', cleaned_text)
+    return cleaned_text
+
+
+def remove_unwanted_chars(sentence):
+
+    """
+        Remove unwanted Characters in-order to clean raw text data in tweets and html docs particularly
+
+        Paramters :
+        -----------------------------------------------------
+        sentence (str)
+    """
+    mentions_pattern = r'@[A-Za-z0-9_]+'
+    http_pattern = r'http(s?)://[^ ]+'
+    www_pattern = r'www.[^ ]+'
+    non_alphabets = r'[^a-zA-Z]+'
+    combined_pattern_removal = r'|'.join((mentions_pattern, http_pattern, www_pattern, non_alphabets))
+    return re.sub(combined_pattern_removal, " ", sentence)
+
