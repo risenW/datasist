f9372d5f752234f35cf2a4b768f9e5d702068a5f
Rising Odegua <risingodegua@gmail.com>
2021-07-24 15:35:31 +0100

    Temporarily comment out nlp

--- datasist/nlp.py
+++ datasist/nlp.py
@@ -1,134 +1,134 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-from nltk.cluster.util import cosine_distance
-import string
-import re
-import numpy as np
-nltk.download('stopwords')
-nltk.download('punkt')
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.stem import PorterStemmer
+# from nltk.tokenize import word_tokenize
+# from nltk.cluster.util import cosine_distance
+# import string
+# import re
+# import numpy as np
+# nltk.download('stopwords')
+# nltk.download('punkt')
 
 
-def convert_lower_case(data):
-    return np.char.lower(data)
+# def convert_lower_case(data):
+#     return np.char.lower(data)
 
 
-def remove_stopwords(data):
-    stop_words = stopwords.words('english')
-    words = word_tokenize(str(data))
-    new_text = ""
-    for w in words:
-        if w not in stop_words and len(w) > 1:
-            new_text = new_text + " " + w
-    return new_text
+# def remove_stopwords(data):
+#     stop_words = stopwords.words('english')
+#     words = word_tokenize(str(data))
+#     new_text = ""
+#     for w in words:
+#         if w not in stop_words and len(w) > 1:
+#             new_text = new_text + " " + w
+#     return new_text
 
 
-def remove_punctuation(mess):
-    nopunc =[char for char in mess if char not in string.punctuation]
-    nopunc=''.join(nopunc)
+# def remove_punctuation(mess):
+#     nopunc =[char for char in mess if char not in string.punctuation]
+#     nopunc=''.join(nopunc)
 
-    return nopunc
-
-
-def remove_apostrophe(data):
-    return np.char.replace(data, "'", "")
+#     return nopunc
 
 
-def stemming(data):
-    stemmer = PorterStemmer()
-
-    tokens = word_tokenize(str(data))
-    new_text = ""
-    for w in tokens:
-        new_text = new_text + " " + stemmer.stem(w)
-    return new_text
+# def remove_apostrophe(data):
+#     return np.char.replace(data, "'", "")
 
 
-def flatten_list(list_of_list):
-    flat_list = [item for sublist in list_of_list for item in sublist]
-    return flat_list
+# def stemming(data):
+#     stemmer = PorterStemmer()
+
+#     tokens = word_tokenize(str(data))
+#     new_text = ""
+#     for w in tokens:
+#         new_text = new_text + " " + stemmer.stem(w)
+#     return new_text
 
 
-def sentence_similarity(sentence1, sentence2, stopwords = nltk.corpus.stopwords.words('english')):
+# def flatten_list(list_of_list):
+#     flat_list = [item for sublist in list_of_list for item in sublist]
+#     return flat_list
+
+
+# def sentence_similarity(sentence1, sentence2, stopwords = nltk.corpus.stopwords.words('english')):
     
-    """ 
-    Finds the Lexical similarity between two sentences based on cosine similarity
+#     """ 
+#     Finds the Lexical similarity between two sentences based on cosine similarity
     
-    Parameters :-
-    ------------------------------------
-          sentence1 (str) : First sentence
-          sentence2 (str) : Second sentence that is compared to the first sentence
-          stopwords (list) : List of stopwords for filtering commonly used words
+#     Parameters :-
+#     ------------------------------------
+#           sentence1 (str) : First sentence
+#           sentence2 (str) : Second sentence that is compared to the first sentence
+#           stopwords (list) : List of stopwords for filtering commonly used words
           
-    Returns :-
-    -------------------------------------
-          similarity (float) : Similarity index between the two sentences represented in a probabilistic form (0.0 <= x <= 1.0)      
-    """
-    if stopwords == None:
-        stopwords = []
+#     Returns :-
+#     -------------------------------------
+#           similarity (float) : Similarity index between the two sentences represented in a probabilistic form (0.0 <= x <= 1.0)      
+#     """
+#     if stopwords == None:
+#         stopwords = []
         
-    assert len(sentence1) > 0 and len(sentence2) > 0, "Each sentence must contain at least one word !"
+#     assert len(sentence1) > 0 and len(sentence2) > 0, "Each sentence must contain at least one word !"
     
 
-    sent1 = [word.lower() for word in sentence1.split(" ")]
-    sent2 = [word.lower() for word in sentence2.split(" ")]
+#     sent1 = [word.lower() for word in sentence1.split(" ")]
+#     sent2 = [word.lower() for word in sentence2.split(" ")]
 
-    all_words = list(set(sent1+sent2))
-    vect_array1 = np.zeros(len(all_words))
-    vect_array2 = np.zeros(len(all_words))
+#     all_words = list(set(sent1+sent2))
+#     vect_array1 = np.zeros(len(all_words))
+#     vect_array2 = np.zeros(len(all_words))
 
-    for word in sent1:
-        if word in stopwords:
-            continue
-        else:
-            vect_array1[all_words.index(word)] += 1
+#     for word in sent1:
+#         if word in stopwords:
+#             continue
+#         else:
+#             vect_array1[all_words.index(word)] += 1
 
-    for word in sent2:
-        if word in stopwords:
-            continue
-        else:
-            vect_array2[all_words.index(word)] += 1
+#     for word in sent2:
+#         if word in stopwords:
+#             continue
+#         else:
+#             vect_array2[all_words.index(word)] += 1
     
-    similarity = np.round(1 - cosine_distance(vect_array1, vect_array2), 3)
+#     similarity = np.round(1 - cosine_distance(vect_array1, vect_array2), 3)
 
-    return similarity
+#     return similarity
 
 
-def remove_newline(text):
-    """
-        Remove newline character and clean text particularly extracted from documents
+# def remove_newline(text):
+#     """
+#         Remove newline character and clean text particularly extracted from documents
 
-        Parameters :
-        --------------------------------------------------
-        text (str) : Text to be cleaned
+#         Parameters :
+#         --------------------------------------------------
+#         text (str) : Text to be cleaned
 
-        Returns :
-        --------------------------------------------------
-        sentence
-    """
-    cleaned_text = []
-    for i in text.split():
-        if i == "\n":
-            continue
-        else: 
-            cleaned_text.append(re.sub(r'[^a-zA-Z0-9_]', "", i))
+#         Returns :
+#         --------------------------------------------------
+#         sentence
+#     """
+#     cleaned_text = []
+#     for i in text.split():
+#         if i == "\n":
+#             continue
+#         else: 
+#             cleaned_text.append(re.sub(r'[^a-zA-Z0-9_]', "", i))
     
-    return " ".join([x for x in cleaned_text if x.strip()])
+#     return " ".join([x for x in cleaned_text if x.strip()])
 
 
-def remove_unwanted_chars(sentence):
+# def remove_unwanted_chars(sentence):
 
-    """
-        Remove unwanted Characters in-order to clean raw text data in tweets and html docs particularly
+#     """
+#         Remove unwanted Characters in-order to clean raw text data in tweets and html docs particularly
 
-        Paramters :
-        -----------------------------------------------------
-        sentence (str)
-    """
-    mentions_pattern = r'@[A-Za-z0-9_]+'
-    http_pattern = r'http(s?)://[^ ]+'
-    www_pattern = r'www.[^ ]+'
-    non_alphabets = r'[^a-zA-Z]+'
-    combined_pattern_removal = r'|'.join((mentions_pattern, http_pattern, www_pattern, non_alphabets))
-    return re.sub(combined_pattern_removal, " ", sentence).strip()
+#         Paramters :
+#         -----------------------------------------------------
+#         sentence (str)
+#     """
+#     mentions_pattern = r'@[A-Za-z0-9_]+'
+#     http_pattern = r'http(s?)://[^ ]+'
+#     www_pattern = r'www.[^ ]+'
+#     non_alphabets = r'[^a-zA-Z]+'
+#     combined_pattern_removal = r'|'.join((mentions_pattern, http_pattern, www_pattern, non_alphabets))
+#     return re.sub(combined_pattern_removal, " ", sentence).strip()
--- datasist/tests/test_nlp.py
+++ datasist/tests/test_nlp.py
@@ -1,67 +1,67 @@
-import pytest
-import numpy as np
-from datasist.nlp import convert_lower_case, remove_stopwords, remove_punctuation, remove_apostrophe, stemming, sentence_similarity, remove_newline, remove_unwanted_chars
+# import pytest
+# import numpy as np
+# from datasist.nlp import convert_lower_case, remove_stopwords, remove_punctuation, remove_apostrophe, stemming, sentence_similarity, remove_newline, remove_unwanted_chars
 
 
-def test_lower_case():
-    text = "DO YOU SHIP TO CANADA?"
-    expected = np.array('do you ship to canada?', dtype='<U22')
-    output = convert_lower_case(text)
+# def test_lower_case():
+#     text = "DO YOU SHIP TO CANADA?"
+#     expected = np.array('do you ship to canada?', dtype='<U22')
+#     output = convert_lower_case(text)
 
-    assert output == expected
-
-
-def test_remove_stopwords():
-    text = "I am a man"
-    expected = " man"
-    output = remove_stopwords(text)
-
-    assert output == expected
+#     assert output == expected
 
 
-def test_remove_punctuation():
-    text = "Hey, I really like this!!!"
-    expected = 'Hey I really like this'
-    output = remove_punctuation(text)
+# def test_remove_stopwords():
+#     text = "I am a man"
+#     expected = " man"
+#     output = remove_stopwords(text)
 
-    assert output == expected
-
-
-def test_remove_apostrophe():
-    text = "This is Daisy's book"
-    expected = np.array('This is Daisys book', dtype='<U19')
-    output = remove_apostrophe(text)
-
-    assert output == expected
+#     assert output == expected
 
 
-def test_stemming():
-    text = "I am loving this"
-    expected = ' i am love thi'
-    output = stemming(text)
+# def test_remove_punctuation():
+#     text = "Hey, I really like this!!!"
+#     expected = 'Hey I really like this'
+#     output = remove_punctuation(text)
 
-    assert output == expected
+#     assert output == expected
 
-def test_sentence_similarity():
-    sentence1 = "Datasist is a very amazing package"
-    sentence2 = "I think Datasist is a very amazing package"
-    expected = 0.866
-    output = sentence_similarity(sentence1, sentence2)
 
-    assert output == expected
+# def test_remove_apostrophe():
+#     text = "This is Daisy's book"
+#     expected = np.array('This is Daisys book', dtype='<U19')
+#     output = remove_apostrophe(text)
 
-def test_remove_newline():
-    text = ")This is a line \n This is a new line ^ -- * 2020"
-    expected = "This is a line This is a new line 2020"
-    output = remove_newline(text)
+#     assert output == expected
 
-    assert output == expected 
 
-def test_remove_unwanted_chars():
-    text = "Me &amp; The Big Homie meanboy3000 #MEANBOY #MB #MBS #MMR #STEGMANLIFE @Stegman St. <url>	"
-    expected = "Me amp The Big Homie meanboy MEANBOY MB MBS MMR STEGMANLIFE Stegman St url"
-    output = remove_unwanted_chars(text)
+# def test_stemming():
+#     text = "I am loving this"
+#     expected = ' i am love thi'
+#     output = stemming(text)
 
-    assert output == expected
+#     assert output == expected
+
+# def test_sentence_similarity():
+#     sentence1 = "Datasist is a very amazing package"
+#     sentence2 = "I think Datasist is a very amazing package"
+#     expected = 0.866
+#     output = sentence_similarity(sentence1, sentence2)
+
+#     assert output == expected
+
+# def test_remove_newline():
+#     text = ")This is a line \n This is a new line ^ -- * 2020"
+#     expected = "This is a line This is a new line 2020"
+#     output = remove_newline(text)
+
+#     assert output == expected 
+
+# def test_remove_unwanted_chars():
+#     text = "Me &amp; The Big Homie meanboy3000 #MEANBOY #MB #MBS #MMR #STEGMANLIFE @Stegman St. <url>	"
+#     expected = "Me amp The Big Homie meanboy MEANBOY MB MBS MMR STEGMANLIFE Stegman St url"
+#     output = remove_unwanted_chars(text)
+
+#     assert output == expected
 
 
